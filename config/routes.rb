d2440cbd46eb24a9a89cae6f1ce135b3eeb44570
Argane Samson <Argane.samson@gmail.com>
2020-08-18 18:28:53 +0200

    create a child good version

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "profil", to: "pages#profil"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :childs, only: [:create, :new, :destroy, :update, :edit] do
-     resources :analyses, only: [:create, :new] 
+  resources :children, only: [:create, :new, :destroy, :update, :edit] do
+    resources :analyses, only: [:create, :new]
   end
-  get "/direction", to: 'analyses#direction'
+  get "/direction", to: "analyses#direction"
   resources :diseases
- 
 end
