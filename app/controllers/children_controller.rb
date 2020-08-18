a5261a5c7f209d3f7506196c2e17a7c3184dfb1b
Argane Samson <Argane.samson@gmail.com>
2020-08-18 21:35:44 +0200

    add column

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,4 +1,6 @@
 class ChildrenController < ApplicationController
+  before_action :set_child, only: [:update, :edit, :destroy]
+
   def new
     @child = Child.new
   end
@@ -12,17 +14,29 @@ class ChildrenController < ApplicationController
   end
 
   def update
+    @child = Child.find(params[:id])
+    @child.update(child_params)
+    redirect_to profil_path
   end
 
   def edit
+    @child = Child.find(params[:id])
   end
 
   def destroy
+    @child = Child.find(params[:id])
+    @child.destroy
+
+    redirect_to profil_path
   end
 
   private
 
   def child_params
     params.require(:child).permit(:first_name, :birth_date, :weight, :cardiac, :respiratory, :prematurity)
+  end
+
+  def set_child
+    @child = Child.find(params[:id])
   end
 end
