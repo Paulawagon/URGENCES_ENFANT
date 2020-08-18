53405eac965dd3fdf70a8ecc03b58e5f23293592
Simon Michel <michelsimontsin2@gmail.com>
2020-08-18 18:09:59 +0200

    demande de commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.new(email: "paula@test.fr", password: "123456", address: "Marseille")
+u1.save!
+
+my_disease = Disease.new(
+	          name: "paula@test.fr",
+              symptoms: "123456",
+              behavior: "Marseille",
+              when_consult: "dqdsfq",
+              need_know: "qsdfa",
+              prevention: "dfqdfq")
+my_disease.user = u1
+my_disease.save!
