4f30b85c2aab9e1024414b800604f8f0c38fdaa8
Argane Samson <Argane.samson@gmail.com>
2020-08-19 16:33:01 +0200

    add API

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,13 +1,5 @@
 class HospitalsController < ApplicationController
   def index
-    @hospitals = Hospital.geocoded # returns flats with coordinates
-
-    @markers = @hospitals.map do |hospital|
-      {
-        lat: hospital.latitude,
-        lng: hospital.longitude,
-      }
-    end
   end
 
   def direction
