f913d091fa90aa1afad588baf0906e3577ade67d
Argane Samson <Argane.samson@gmail.com>
2020-08-19 09:25:52 +0200

    promise this is the good version

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,10 @@ Child.destroy_all
 User.destroy_all
 Disease.destroy_all
 
-
-u1 = User.new(first_name: "Paula" last_name: "Pisa" email: "paula@test.fr", password: "123456")
+u1 = User.new(first_name: "Paula", last_name: "Pisa", email: "paula@test.fr", password: "123456")
 u1.save!
 
-e1 = Child.new(first_name: "Lola", birth_date: "2019, 11, 10", weight:500, cardiac: false, respiratory: false, prematurity: false)
+e1 = Child.new(first_name: "Lola", birth_date: "2019, 11, 10", weight: 500, cardiac: false, respiratory: false, prematurity: false)
 e1.user = u1
 e1.save!
 
@@ -22,8 +21,5 @@ a1 = Analyse.new(symptoms: ["toux"])
 a1.child = e1
 a1.save!
 
-
-
-d1 = Disease.new(name:"Fièvre",symptoms: ["fièvre"], behavior: " Doliprane", when_consult:"Après 48h de fièvre", need_know: "peut pas etre blank", prevention: "peut pas etre blank")
+d1 = Disease.new(name: "Fièvre", symptoms: ["fièvre"], behavior: " Doliprane", when_consult: "Après 48h de fièvre", need_know: "peut pas etre blank", prevention: "peut pas etre blank")
 d1.save!
-
