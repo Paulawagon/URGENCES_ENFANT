16fbe98af6d86434a78ca829966c42373e3f1bb0
Paula Pisa <pisa.paula@gmail.com>
2020-08-19 16:02:19 +0200

    Checkboxes and tests on analysis

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -3,9 +3,27 @@ class AnalysesController < ApplicationController
   end
 
   def new
-    @analysis = Child.new
+    @analysis = Analysis.new
+    @child = Child.find(params[:child_id])
+    @symptoms = ["Fièvre","Toux","Rhume","Diarrhées","Vomissements","Douleurs abdominales","Brûlures"]
   end
+
+  def create
+    @analysis = Analysis.new
+    @child = Child.find(params[:child_id])
+    @analysis.child = @child
+    @analysis.save
+    if @analysis.is_an_emergency?
+      redirect_to direction_path
+    else 
+      redirect_to disease_path
+    end
+
+
+  end
+
 
   def direction
   end
+
 end
