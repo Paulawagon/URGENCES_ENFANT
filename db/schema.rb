07d8c3ff5728828d5f8761f355cf1bda1bcda846
Simon Michel <michelsimontsin2@gmail.com>
2020-08-21 12:42:21 +0200

    sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,6 @@ ActiveRecord::Schema.define(version: 2020_08_21_093442) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -66,6 +64,8 @@ ActiveRecord::Schema.define(version: 2020_08_21_093442) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
