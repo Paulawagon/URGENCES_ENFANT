8e506fdf779244d4ba2d56ffaae00d802db653ce
Paula Pisa <pisa.paula@gmail.com>
2020-08-21 10:38:13 +0200

    Seed additional infos

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,48 +29,54 @@ a1 = Analysis.new(symptoms: ["toux"])
 a1.child = e1
 a1.save!
 
-d1 = Disease.new(name: "Fièvre", symptoms: ["fièvre"], behavior: " Découvrez votre enfant (vêtements légers dans une pièce aérée non surchaufée -18-20°.\n
-Donnez lui à boire +++.\n
+d1 = Disease.new(name: "Fièvre", symptoms: ["fièvre"], behavior: " Découvrez votre enfant et ne surchauffez pas la pièce (entre 18 et 20°C maximum).\n
+Donnez lui fréquemment à boire +++.\n
 Ces mesures simples sont suffisantes dans bien des cas !\n
-Les bains tièdes et les enveloppements de linges humides ne sont plus recommendés.\n
-Les médicaments : Avant l’avis de votre médecin traitant, ne donnez qu’un seul médicament antipyrétique (=contre la fièvre) à la fois :Paracétamol (doliprane, efferalgan, dolko, dafalgan) toutes les 6 heures (privilégiez la voie orale). Attention de ne pas associer plusieurs médicaments contenant les mêmes molécules(ou des molécules de la même famille) ex :doliprane et efferalgan", when_consult: "Après 48 heures en suivant les mesures préconnisées dans la conduite à tenir.\n
-Si votre enfant est très endormi difficile à tirer du sommeil, s’il a une éruption cutanée, une gêne respiratoire.\n
-S’il manifeste d’autres symptômes qui vous inquiètent", need_know: "On considère qu’un enfant a de la fièvre quand sa température dépasse 38°C. Généralement, ce n’est qu’au-dessus de 38,5°C que l’on envisage un traitement. \n
+Les bains tièdes et les enveloppements de linges humides ne sont plus recommandés.\n
+Les médicaments : Avant l’avis de votre médecin traitant, ne donnez qu’un seul médicament antipyrétique (=contre la fièvre) à la fois : le paracétamol (Nom commercial : doliprane, efferalgan, dolko, dafalgan) toutes les 6 heures si besoin. Attention de ne pas associer plusieurs médicaments contenant les mêmes molécules (ou des molécules de la même famille) ex :doliprane et efferalgan", when_consult: "Consultez aux Urgences si vous n'arrivez pas à réveiller votre enfant, s’il a une éruption cutanée, une difficulté respiratoire, une coloration bleue des lèvres ou des extrêmités, des tremblements ou des convulsions.\n
+En cas de convulsions (perte de contact, modification du tonus, mouvements anormaux des yeux ou des membres) GARDEZ votre calme, allongez votre enfant sur le côté afin de dégager ses voies respiratoires, éloignez le de tout objet susceptible de le blesser.\n
+N'essayez pas de lui ouvrir la bouche. Si possible notez la durée des convulsions, elles s'arrêtent généralement d'elles-mêmes en quelques minutes. Si ce n'est pas le cas et que votre enfant ne se reveille pas complètement au delà de 10 minutes après le début des convulsions appeler le 15.\n
+Si votre enfant ne présente pas de signes inquiètants, vous pouvez consulter au bout de 48 heures votre médecin traitant ou les urgences en cas d'impossibilité .\n", need_know: " La température se mesure avec un thermomètre (au mieux en rectal). La notion ‘il est chaud’ ne veut rien dire. On considère qu’un enfant a de la fièvre quand sa température dépasse 38°C. Généralement, ce n’est qu’au-dessus de 38,5°C que l’on envisage un traitement. \n
 Il n’est pas nécessaire de traiter systématiquement la fièvre surtout si elle est bien supportée par l’enfant. Le but du traitement n’est pas de normaliser à tout prix la température, mais d’améliorer le confort de l’enfant. \n
-L’ampleur de la fièvre n’est pas corrélée à la maladie, le comportement de l’enfant est plus révélateur (en effet, on peut observer une forte fièvre chez un enfant atteint d’une infection bénigne et ne relever aucune élévation de température chez un enfant souffrant d’une très grave infection. \n
-La fièvre fait partie d’un processus complexe de défense de l’organisme, c’est une réponse aux infections : les agents infectieux ont tendance à moins bien se développer lorsque la température s’élève et les cellules de défense de l’organisme (les lymphocytes T et B) sont plus efficaces lorsque le corps est à 39°C (certaines publications indiquent même que le traitement de la fièvre retarderait la guérison de certaines infections virales !).\n
-Les convulsions : elles ne sont pas systématiques dès que la température s’élève. Seuls certains enfants (souvent avec un caractère familial) ont une susceptibilité particulière et peuvent convulser (et pour eux, même avant 39°C !). Ces convulsions sont sans gravité, ne durent que quelques instants mais sont très impressionnantes !\n
-La température se mesure avec un thermomètre (au mieux en rectal). La notion ‘il est chaud’ ne veut rien dire", prevention: "Réaction de défense face à un virus")
+L’ampleur de la fièvre n’est pas corrélée à la gravité de la maladie, le comportement de l’enfant est plus révélateur (en effet, on peut observer une forte fièvre chez un enfant atteint d’une infection bénigne et ne relever aucune élévation de température chez un enfant souffrant d’une très grave infection. \n
+La fièvre fait partie d’un processus complexe de défense de l’organisme, c’est une réponse aux infections).\n
+Seuls certains enfants (souvent avec un caractère familial) ont une susceptibilité particulière et peuvent convulser, même en cas de fièvre peu élevée). Ces convulsions sont généralement sans gravité, même si elles sont très impressionnantes !\n
+", prevention: "La fièvre est une réaction de défense face à un microbe, pour s'en protéger et éviter la contamination: lavez vous régulièrement les mains avec du savon ou une solution hydroalcoolique.\n
+Evitez, quand cela est possible, d’emmener votre enfant malade ou ayant moins de 3 mois dans les endroits publics confinés (transports en commun, centres commerciaux, etc.).\n
+Ne pas partager les biberons, sucettes ou couverts.\n
+Ouvrez les fenêtres de la pièce où il dort au moins 10 minutes par jour pour aérer.\n
+Lorsque l’on est soi-même enrhumé se couvrir la bouche lorsque l’on tousse avec son coude ou sa manche. Portez un masque lorsque l’on s’occupe de son bébé. Evitez d’embrasser son enfant sur le visage et sur les mains. ")
 
 d1.save!
 
-d2 = Disease.new(name: "Toux", symptoms: ["toux"], behavior: " Les médicaments contre la toux ne servent à rien et ne sont pas conseillés. Nettoyer le nez au moins 6 fois par jour avec du sérum physiologique, en particulier avant de lui donner à boire ou à manger.\n
+d2 = Disease.new(name: "Toux", symptoms: ["toux"], behavior: " Les médicaments contre la toux ne servent à rien et ne sont pas conseillés car ils peuvent avoir des effets secondaires dangereux. Ne donnez jamais de miel à un enfant de moins de 1 an (risque de botulisme). \n
+Même s'il ne vous paraît pas enrhumé, nettoyer lui le nez au moins 6 fois par jour avec du sérum physiologique, en particulier avant de lui donner à boire ou à manger.\n
 Lui donner régulièrement de l’eau à boire pour éviter la déshydratation.\n
-Fractionner  ses repas (lui donner à manger plus souvent et en plus petites quantités).\n
+Fractionnez  ses repas (lui donner à manger plus souvent et en plus petites quantités).\n
 Bien aérer toutes les pièces du logement ( particulièrement la pièce où il dort).\n
 Ne pas trop le couvrir.\n
 Continuer à le coucher sur le dos à plat.\n
 Ne jamais fumer près de lui. ", when_consult: "Il est préférable de se rendre rapidement aux urgences : \n
-Si votre enfant boit moins de la moitié de ses biberons à trois repas consécutifs.\n
-Il vomit systématiquement.\n
-Il dort en permanence, ou au contraire, pleure de manière inhabituelle et ne peut s’endormir.\n
-Sa respiration est détériorée : il fait plus de bruit en respirant, il tousse davantage, sa respiration est plus rapide (plus de 40 respirations par minutes). On observe un battement des ailes du nez. On note un tirage intercostal ( le thorax se creuse).
-Sinon consultez votre médecin traitant qui examinera votre enfant et prescrira les soins nécessaires", need_know: " La bronchiolite débute par un simple rhume et l’enfant tousse un peu. Puis la toux est plus fréquente, la respiration peut devenir sifflante. L’enfant peut être gêné pour respirer et avoir du mal à manger et à dormir. Il peut avoir de la fièvre.\n
-Dans la majorité des cas, la bronchiolite guérit spontanément au bout de 5 à 10 jour mais peut persister pendant 2 à 4 semaines.La bronchiololite est une maladie respiratoire très fréquentes chez les nourrissons et les enfants de moins de 2 ans. Elle est due le plus souvent à un virus appelé Virus Respiratoire Syndical (VRS) qui touche les petites bronches.\n
-Les épidémies de bronchiolite débutent généralement au cours du mois d’octobre et se terminent courant janvier.", prevention: "La bronchiolite est une maladie très contagieuse.\n
+En cas de suspicion d'inhalation de corps étranger.
+Si votre enfant mange moins de la moitié des quantités habituelles sur la journée, ou qu'il vomit systématiquement.\n
+Si vous n'arrivez pas à réveiller votre enfant, s'il présente une coloration bleue des lèvres ou des extrêmités ou une difficulté respiratoire : sa respiration est très rapide ou fait un bruit anormal, ses muscles du cou ou du thorax se contractent pour respirer, son thorax se creuse.
+Si votre enfant ne présente pas de signes inquiètants, consultez votre médecin traitant ou les urgences en cas d'impossibilité", need_know: " La toux est une réaction de défense du système respiratoire afin d'expulser un agent infectieux ou un corps étranger. Il ne faut pas tenter de la stopper, même si elle est gênante.\n
+La bronchiolite est une cause fréquente chez l'enfant, elle débute par un simple rhume, puis la toux devient plus fréquente, la respiration peut devenir sifflante. L’enfant peut être gêné pour respirer et avoir du mal à manger. Il peut avoir de la fièvre.\n
+Dans la majorité des cas, la bronchiolite guérit spontanément au bout de 5 à 10 jours mais peut persister pendant 2 à 4 semaines. La bronchiololite est une maladie respiratoire très fréquente chez les enfants de moins de 2 ans. Elle est due le plus souvent à un virus appelé Virus Respiratoire Syndical (VRS) qui touche les petites bronches.", prevention: "La bronchiolite est une maladie très contagieuse.\n
 Les adultes et les grands enfants qui sont porteurs du virus respiratoire syncytial n’ont habituellement aucun signe ou ont un simple rhume. Le virus se transmet facilement par la salive, la toux, et les éternuements.\n
-Le virus peut rester sur les mains et les objects (comme sur les jouets, les tétines, les « doudous ».\n
-Se laver les mains pendant 30 secondes avec de l’eau et du savon avant et après un change, une têtée, calîns, biberon, repas, etc. ou en utilisant une solution hydroalcoolique.\n
-Evitez, quand cela est possible, d’emmener son enfant dans les endroits publics confinés (transports en commun, centres commerciaux, etc.) où il risquerait d’être en contact avec des personnes enrhumées.\n
-Ne pas partager les biberons, sucettes ou couverts non lavés.\n
-Lavez régulièrement jouets et doudous.
-Ouvrez les fenêtres de la pièce où il dort au moins 10 minutes par jour pour aérer. Maintenir la température de la pièce aux alentours de 19°C\n
-En ne fumant pas à côté des bébés et des enfants.\n
-Lorsque l’on est soi-même enrhumé se couvrir la bouche lorsque l’on tousse avec son coude ou sa manche. Portez un masque lorsque l’on s’occupe de son bébé. Evitez d’embrasser son enfant sur le visage et sur les mains.")
+Il peut rester sur les mains et les objets.\n
+Lavez vous régulièrement les mains avec du savon ou une solution hydroalcoolique.Lavez régulièrement jouets et doudous.\n
+Evitez, quand cela est possible, d’emmener votre enfant malade ou ayant moins de 3 mois dans les endroits publics confinés (transports en commun, centres commerciaux, etc.).\n
+Ne pas partager les biberons, sucettes ou couverts.\n
+Ouvrez les fenêtres de la pièce où il dort au moins 10 minutes par jour pour aérer.\n
+Maintenir la température de la pièce aux alentours de 19°C\n
+Ne pas fumer à côté des bébés et des enfants.
+Lorsque l’on est soi-même enrhumé se couvrir la bouche lorsque l’on tousse avec son coude ou sa manche. Portez un masque lorsque l’on s’occupe de son bébé. Evitez d’embrasser son enfant sur le visage et sur les mains.\n
+")
 
 d2.save!
 
-d3 = Disease.new(name: "Rhume", symptoms: ["toux"], behavior: " Nettoyer le nez au moins 6 fois par jour avec du sérum physiologique, en particulier avant de lui donner à boire ou à manger.\n
+d3 = Disease.new(name: "Rhume", symptoms: ["rhume"], behavior: " Nettoyer le nez au moins 6 fois par jour avec du sérum physiologique, en particulier avant de lui donner à boire ou à manger.\n
 Lui donner régulièrement de l’eau à boire pour éviter la déshydratation.\n
 Fractionner  ses repas (lui donner à manger plus souvent et en plus petites quantités).\n
 Bien aérer toutes les pièces du logement ( particulièrement la pièce où il dort).\n
@@ -79,7 +85,7 @@ Ne jamais fumer près de lui. ", when_consult: "Il est préférable de se rendre
 Si votre enfant boit moins de la moitié de ses biberons à trois repas consécutifs.\n
 Il vomit systématiquement.\n
 Il dort en permanence, ou au contraire, pleure de manière inhabituelle et ne peut s’endormir.\n
-Sinon consultez votre médecin traitant qui examinera votre enfant et prescrira les soins nécessaires", need_know: " La bronchiolite peut débuter par un simple rhume et l’enfant tousse un peu. Puis la toux est plus fréquente, la respiration peut devenir sifflante. L’enfant peut être gêné pour respirer et avoir du mal à manger et à dormir. Il peut avoir de la fièvre.\n
+Sinon consultez votre médecin traitant.", need_know: " La bronchiolite peut débuter par un simple rhume et l’enfant tousse un peu. Puis la toux est plus fréquente, la respiration peut devenir sifflante. L’enfant peut être gêné pour respirer et avoir du mal à manger et à dormir. Il peut avoir de la fièvre.\n
 Dans la majorité des cas, la bronchiolite guérit spontanément au bout de 5 à 10 jour mais peut persister pendant 2 à 4 semaines.La bronchiololite est une maladie respiratoire très fréquentes chez les nourrissons et les enfants de moins de 2 ans. Elle est due le plus souvent à un virus appelé Virus Respiratoire Syndical (VRS) qui touche les petites bronches.\n
 Les épidémies de bronchiolite débutent généralement au cours du mois d’octobre et se terminent courant janvier.", prevention: "La bronchiolite est une maladie très contagieuse.\n
 Les adultes et les grands enfants qui sont porteurs du virus respiratoire syncytial n’ont habituellement aucun signe ou ont un simple rhume. Le virus se transmet facilement par la salive, la toux, et les éternuements.\n
